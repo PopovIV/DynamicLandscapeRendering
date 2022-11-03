1545b6c2f08089a7a3427e26db1f7fe31148b692
PopovIV <71888203+PopovIV@users.noreply.github.com>
2022-11-03 07:03:07 +0300

    Added view based tessellation

--- Terrain/TerrainHullShader.hlsl
+++ Terrain/TerrainHullShader.hlsl
@@ -18,6 +18,13 @@ struct DS_INPUT
     float4 position : POSITION;
 };
 
+float calcTessFactor(float3 p) {
+    float d = distance(p, cameraPos);
+
+    float s = saturate((d - 16.0f) / (512.0f - 16.0f));
+    return pow(2, (lerp(6, 0, s)));
+}
+
 struct HS_CONSTANT_DATA_OUTPUT
 {
     float EdgeFactors[4]         : SV_TessFactor;
@@ -28,19 +35,30 @@ HS_CONSTANT_DATA_OUTPUT constantsHullShader(InputPatch<HS_INPUT, NUM_CONTROL_POI
 {
     HS_CONSTANT_DATA_OUTPUT output;
 
-    // Assign tessellation levels (constant for now)
-    output.EdgeFactors[0] = 1;
-    output.EdgeFactors[1] = 1;
-    output.EdgeFactors[2] = 1;
-    output.EdgeFactors[3] = 1;
-    output.InsideFactor[0] = 1;
-    output.InsideFactor[1] = 1;
+    float3 v0 = mul(patch[0].position, worldMatrix);
+    float3 v1 = mul(patch[1].position, worldMatrix);
+    float3 v2 = mul(patch[2].position, worldMatrix);
+    float3 v3 = mul(patch[3].position, worldMatrix);
+
+    float3 e0 = 0.5f * (v0 + v2);
+    float3 e1 = 0.5f * (v0 + v1);
+    float3 e2 = 0.5f * (v1 + v3);
+    float3 e3 = 0.5f * (v2 + v3);
+    float3 c = 0.25f * (v0 + v1 + v2 + v3);
+
+    // Assign tessellation levels
+    output.EdgeFactors[0] = calcTessFactor(e0);
+    output.EdgeFactors[1] = calcTessFactor(e1);
+    output.EdgeFactors[2] = calcTessFactor(e2);
+    output.EdgeFactors[3] = calcTessFactor(e3);
+    output.InsideFactor[0] = calcTessFactor(c);
+    output.InsideFactor[1] = output.InsideFactor[0];
 
     return output;
 }
 
 [domain("quad")]
-[partitioning("fractional_odd")]
+[partitioning("fractional_even")]
 [outputtopology("triangle_cw")]
 [outputcontrolpoints(NUM_CONTROL_POINTS)]
 [patchconstantfunc("constantsHullShader")]
--- Terrain/zone.cpp
+++ Terrain/zone.cpp
@@ -50,7 +50,7 @@ bool Zone::Initialize(D3DClass* Direct3D, HWND hwnd, int screenWidth, int screen
         return false;
 
     // Set the initial position and rotation.
-    m_Position->SetPosition(512.0f, 30.0f, -10.0f);
+    m_Position->SetPosition(512.0f, 110.0f, 0.0f);
     m_Position->SetRotation(0.0f, 0.0f, 0.0f);
 
     // Create the terrain object.
@@ -196,7 +196,7 @@ bool Zone::Render(D3DClass* Direct3D, ShaderManager* ShaderManager, TextureManag
     Direct3D->GetOrthoMatrix(orthoMatrix);
 
     // Clear the buffers to begin the scene.
-    Direct3D->BeginScene(0.2f, 0.2f, 0.5f, 1.0f);
+    Direct3D->BeginScene(0.0f, 0.50f, 1.0f, 1.0f);
 
     // Determine if the terrain should be rendered in wireframe or not.
     if (m_wireFrame)
